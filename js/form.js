39061807c558dbb49828e0ead54b4e914f54a2a8
Amna286 <75910591+Amna286@users.noreply.github.com>
2021-08-10 19:03:37 +0100

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,60 @@
+class Form{
+    constructor(){
+       this.input = createInput("Name");
+       this.button = createButton('Play');
+       this.greeting = createElement('h2');
+       this.title = createElement('h2');
+       this.reset = createButton('Reset');
+    }
+    hide() {
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        this.title.hide();
+    }
+    display() {
+        this.title.html("FRUIT CATCHER");
+        this.title.position(windowWidth/2-282.5, windowHeight-800);
+        this.title.style('font-size', '70px');
+        this.title.style('color', 'skyblue');
+        this.input.position(windowWidth/2-95, windowHeight-500);
+        this.input.style('width', '200px');
+        this.input.style('height', '20px');
+        this.input.style('background', 'lavender');
+        this.button.position(windowWidth/2-90, windowHeight-450);
+        this.button.style('width', '200px');
+        this.button.style('height', '40px');
+        this.button.style('background', 'lightpink');
+        this.reset.position(windowWidth/2-40, windowHeight-220);
+        this.reset.style('width', '100px');
+        this.reset.style('height', '30px');
+        this.reset.style('background', 'lightpink');
+
+        this.button.mousePressed(() => {
+            this.input.hide();
+            this.button.hide();
+            player.name = this.input.value();
+            playerCount += 1;
+            player.index = playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            this.greeting.html("Hello " + player.name)
+            this.greeting.position(windowWidth/2-230, windowHeight-600);
+            this.greeting.style('color', 'white');
+            this.greeting.style('font-size', '100px');
+        });
+
+        this.reset.mousePressed(() => {
+            //add code to reset the values of the gameState and the playerCount nodes to 0 in the database
+            player.updateCount(0);
+            game.update(0);
+            //playerInfoRef.remove();
+
+            push();
+            fill("yellow");
+            textSize(10);
+            text("Please refresh", this.reset.position.x, this.reset.position.y+20);
+            pop();
+        });
+    }
+}
